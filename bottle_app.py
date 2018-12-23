4e51bc4ae6a7f53fa98d7107d79a726f2b5e059b
AhmetAlperYilmaz <alperyilmaz2000@hotmail.com>
2018-12-23 10:10:46 +0300

    .

--- bottle_app.py
+++ bottle_app.py
@@ -6,7 +6,16 @@
 #####################################################################
 
 from bottle import route, run, default_app, debug, static_file
+from hashlib import sha256
 
+mypassword = "219aeb43c0cc62089487cc77c6603b760edac4d616186e6fea5d0aa8122f49c2"
+#Password Protection has been taken from this link:
+#https://bitbucket.org/damienjadeduff/hashing_example/raw/master/hash_password.py
+
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
+    
 def htmlify(title,text):
     page = """
         <!doctype html>
@@ -18,13 +27,35 @@ def htmlify(title,text):
             <body>
             %s
             </body>
+            <div><input type="text" style="width: 50"/>><button>Add Comment</button></div>
         </html>
     """ % (title,text)
     return page
 
 def index():
-    return htmlify("",
-                   "This is going to be an awesome website, when it is finished.")
+    return htmlify("Commentable Website",
+                   "Welcome to the program which memorises your comments.")
+
+your_comments_list = []
+while (len(your_comments_list) >= 0):
+    listing_number = 1
+    your_input = input("Please enter your comment or if you want to exit from the program please write 'exit': ")
+    
+    if (your_input == "exit"):
+    	break
+    
+    passwordconfirming = input("Please enter your password to memorise your comments.")
+    password_hash = create_hash(passwordconfirming)
+    
+    if (mypassword == password_hash):
+      print("Your password has been confirmed.")
+    
+      your_comments_list.append(your_input)
+    print("All of your comments are: ")
+    for one_of_your_comment in your_comments_list:
+         print(str(listing_number) + "-) " + one_of_your_comment)
+         listing_number += 1
+
 
 
 def static_file_callback(filename):
@@ -48,4 +79,3 @@ app = default_app()
 # The below code is necessary for running this bottle app standalone on your computer.
 if __name__ == "__main__":
   run()
-
