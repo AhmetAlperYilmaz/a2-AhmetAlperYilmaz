9b5a930567fd2c497529ad4db7c73768221a71d0
AhmetAlperYilmaz <alperyilmaz2000@hotmail.com>
2018-12-25 19:12:35 +0300

    .

--- repl_comments.py
+++ repl_comments.py
@@ -11,23 +11,23 @@ def create_hash(password):
     return sha256(pw_bytestring).hexdigest()
     
 your_comments_list = []
-while (len(your_comments_list) >= 0):
+while(len(your_comments_list >=0)):
     listing_number = 1
     your_input = input("Please enter your comment or if you want to exit from the program please write 'exit': ")
     
     if (your_input == "exit"):
-		break
+    	break
     
     passwordconfirming = input("Please enter your password to memorise your comments.")
     password_hash = create_hash(passwordconfirming)
     
     if (mypassword == password_hash):
-		print("Your password has been confirmed.")
-		your_comments_list.append(your_input)
-		print("All of your comments are: ")
-		for one_of_your_comment in your_comments_list:
-			print(str(listing_number) + "-) " + one_of_your_comment)
-			listing_number += 1
-	else:
-		print("This password is wrong, please try again.")
+    	print("Your password has been confirmed.")
+    	your_comments_list.append(your_input)
+    	print("All of your comments are: ")
+    	for one_of_your_comment in your_comments_list:
+        	print(str(listing_number) + "-) " + one_of_your_comment)
+        	listing_number += 1
+    else:
+        print("This password is wrong, please try again.")
 
